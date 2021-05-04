be123b7994061cfbc82f3b43a06a449cdb596956
athewarkar <69321540+athewarkar@users.noreply.github.com>
2021-05-04 00:57:57 -0400

    Create TurtleP2.py

---
+++ TurtleP2.py
@@ -0,0 +1,46 @@
+#TurtleP2.py
+
+import turtle
+import math
+
+ascreen = turtle.Screen()
+aturtle = turtle.Turtle()
+aturtle.up()
+aturtle.goto(-100,0)
+aturtle.down()
+aturtle.forward(100)
+aturtle.left(120)
+aturtle.forward(100)
+aturtle.left(120)
+aturtle.forward(100)
+aturtle.left(120)
+
+# move turtle for another object
+# Rectangle
+aturtle.up()
+aturtle.forward(200)
+aturtle.down()
+aturtle.forward(100)
+aturtle.left(90)
+aturtle.forward(100)
+aturtle.left(90)
+aturtle.forward(100)
+aturtle.left(90)
+aturtle.forward(100)
+aturtle.left(90)
+
+# Pentagon
+
+aturtle.up()
+aturtle.forward(200)
+aturtle.down()
+aturtle.forward(100)
+aturtle.left(72)
+aturtle.forward(100)
+aturtle.left(72)
+aturtle.forward(100)
+aturtle.left(72)
+aturtle.forward(100)
+aturtle.left(72)
+aturtle.forward(100)
+aturtle.left(72)
