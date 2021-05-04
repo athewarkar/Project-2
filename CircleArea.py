9f0dfc37709f151d4ae62868ea45f74d87e6f54b
athewarkar <69321540+athewarkar@users.noreply.github.com>
2021-05-04 00:58:30 -0400

    Update and rename README.md to CircleArea.py

--- CircleArea.py
+++ CircleArea.py
@@ -36,11 +36,7 @@ print("Area of a Square: %.2f" %areaSquare)
 
 
 
-#Pytube.py
 
-![image](https://user-images.githubusercontent.com/69321540/116961495-79391c80-ac71-11eb-9ca4-244ecd9ce6ad.png)
-![image](https://user-images.githubusercontent.com/69321540/116961512-80602a80-ac71-11eb-9d6d-4c8b2c10c35b.png)
-![image](https://user-images.githubusercontent.com/69321540/116961517-835b1b00-ac71-11eb-87a6-c7e22ff9ba53.png)
 
 
 
